ef7a97c130fea4c633d2449b59867cf82c576556
Michael Barton <mail@bioinformaticszen.com>
2008-04-22 18:25:31 +0100

    Created repository

---
+++ code/migrations/001_create_genes_table.rb
@@ -0,0 +1,12 @@
+class CreateGenesTable < ActiveRecord::Migration
+  def self.up
+    create_table :genes do |t|
+      t.column :name,     :string
+      t.column :sequence, :string
+    end
+  end
+
+  def self.down
+    drop_table :genes
+  end
+end
