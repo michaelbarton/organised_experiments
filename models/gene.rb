e3a3139394707b6219c700f24535f5b619756fe9
Michael Barton <mail@bioinformaticszen.com>
2008-04-26 18:16:13 +0100

    Created method to load data into database from Bio::FlatFile entry.

--- models/gene.rb
+++ models/gene.rb
@@ -1,2 +1,10 @@
 class Gene < ActiveRecord::Base
+
+  def self.create_from_flatfile(entry)
+    Gene.create(
+      :name      =>  entry.definition.split(/\s+/).first,
+      :sequence  =>  entry.data     
+    )
+  end
+
 end
