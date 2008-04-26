27e8728bf4766115a3fb0e6ec9533fa90780492f
Michael Barton <mail@bioinformaticszen.com>
2008-04-26 20:17:10 +0100

    Added method to bin gene length into frequency table

--- models/gene.rb
+++ models/gene.rb
@@ -20,4 +20,23 @@ class Gene < ActiveRecord::Base
     genes.map{|gene| gene.sequence.length}.to_statarray.stddev
   end
 
+  def self.bin_sequence_length(n_bins)
+    lengths = Gene.find(:all).map{|gene| gene.sequence.length}
+    min = lengths.min
+    max = lengths.max
+    bin_size = (max - min) / n_bins
+
+    frequencies = Hash.new
+    n_bins.times do |i|
+      frequencies.store(Range.new(i*bin_size + min, i*bin_size + bin_size + min, true) , 0)
+    end
+
+    lengths.each do |length|
+      frequencies.each_key { |range| frequencies[range] += 1 if range.include? length }
+    end
+
+    frequencies
+    
+  end
+
 end
